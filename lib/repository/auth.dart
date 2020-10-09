e67c9048d25a591215f2f2dddfac006b527b0c7b
Hari Om <58305594+Madhav008@users.noreply.github.com>
2020-10-10 01:23:06 +0530

    new changes

--- lib/repository/auth.dart
+++ lib/repository/auth.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:goldnumber/model/game_data.dart';
 import 'package:goldnumber/model/recent_game.dart';
 import 'package:http/http.dart' as http;
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:goldnumber/bloc/games_bloc.dart';
 import 'package:goldnumber/bloc/recent_games_bloc.dart';
-import 'package:goldnumber/model/game_data.dart';
-import 'package:goldnumber/model/recent_game.dart';
 import 'package:goldnumber/widget/numbers.dart';
+import 'package:goldnumber/widget/recent_number.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
@@ -34,43 +33,11 @@ class _HomeState extends State<Home> {
       body: Column(
         children: [
           Expanded(
-              child: Container(
-                  child: StreamBuilder<List<RecentGame>>(
-                      stream: recentGameList.subject.stream,
-                      builder: (context, snapshot) {
-                        return GridView.count(
-                          crossAxisCount: 1,
-                          scrollDirection: Axis.horizontal,
-                          children:
-                              List.generate(snapshot.data.length, (index) {
-                            return Card(
-                              color: Colors.yellow[200],
-                              elevation: 0.2,
-                              child: Column(
-                                children: [
-                                  Center(
-                                      child: Text(
-                                    snapshot.data[index].tittle,
-                                    maxLines: 1,
-                                    style: TextStyle(
-                                        fontSize: 25, color: Colors.black),
-                                  )),
-                                  Center(
-                                      child: Text(
-                                          "NEW => " +
-                                              snapshot.data[index].newResult,
-                                          style: TextStyle(
-                                              fontSize: 20,
-                                              color: Colors.green))),
-                                ],
-                              ),
-                            );
-                          }),
-                        );
-                      }))),
+              child: RecentGames()),
           Expanded(flex: 2, child: GuessNumber()),
         ],
       ),
     );
   }
 }
+
